0bcc3bc8e5f692c9aab66e4070fedf1717dd99ae
Adrian JUNG <ajung@e1r11p23.42.fr>
2021-12-01 22:47:23 +0100

    push auto

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,40 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:26:57 by ajung             #+#    #+#             */
-/*   Updated: 2021/12/01 15:26:58 by ajung            ###   ########.fr       */
+/*   Updated: 2021/12/01 22:46:03 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+char	*get_next_line(int fd)
+{
+	int		i;
+	char	*output;
+	
+	static char	*buffer[BUFFER_SIZE + 1]; //char **?
+	//static int start_line = 0; //trouver comment read a partir d'un index?
+	
+	while (1)
+	{
+		i = 0;
+		read(fd, buffer, BUFFER_SIZE); // ssize_t read(int fd, void *buf, size_t nbyte);
+		buffer[BUFFER_SIZE] = '\0';
+		while (buffer[i] != '\0' || buffer[i] != '\n')
+			i++;
+		if (buffer[i] == '\n')
+		{
+			output = ft_strjoin(output, buffer, i); //strnjoin??
+			return (output);
+		}
+		else if (buffer[i] == '\0')
+		{
+			output = ft_strjoin(output, buffer, i - 1); //strnjoin??
+			return (output);
+		}
+		else
+		{
+			output = ft_strjoin(output, buffer, BUFFER_SIZE);
+		}
+	}
+}
--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -6,7 +6,61 @@
 /*   By: ajung <ajung@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/01 15:25:52 by ajung             #+#    #+#             */
-/*   Updated: 2021/12/01 15:25:53 by ajung            ###   ########.fr       */
+/*   Updated: 2021/12/01 17:16:56 by ajung            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "get_next_line.h"
+
+
+
+char	*ft_strchr(const char *s, int c)
+{
+	unsigned int	i;
+	unsigned char	p;
+
+	p = (unsigned char) c;
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == p)
+			return ((char *)&(s[i]));
+		i++;
+	}
+	if (p == 0)
+		return ((char *)&(s[i]));
+	return (NULL);
+}
+
+size_t	ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	int		i;
+	int		j;
+	char	*output;
+
+	if (!s1 || !s2)
+		return (NULL);
+	i = 0;
+	j = 0;
+	output = (char *)malloc(sizeof(char)
+			* (ft_strlen(s1) + ft_strlen(s2)+ 1));
+	if (output == NULL)
+		return (NULL);
+	while (s1[i])
+		output[j++] = s1[i++];
+	i = 0;
+	while (s2[i])
+		output[j++] = s2[i++];
+	output[j] = '\0';
+	return (output);
+}
